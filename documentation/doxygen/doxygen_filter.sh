58fe6e7d404c1ecf51331cf470aec3cffdfc0fd4
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2014-07-25 10:06:48 +0100

    adding html documentation to revision control

---
+++ documentation/doxygen/doxygen_filter.sh
@@ -0,0 +1,2 @@
+#!/bin/env sh
+PYTHONPATH=/dls_sw/prod/common/python/RHEL6-x86_64/iocbuilder/3-40/prefix/lib/python2.7/site-packages/iocbuilder-3.40-py2.7.egg /dls_sw/prod/tools/RHEL6-x86_64/Python/2-7-3/prefix/bin/python2.7 /dls_sw/prod/common/python/RHEL6-x86_64/dls_scripts/3-11/prefix/lib/python2.7/site-packages/dls_scripts-3.11-py2.7.egg/dls_scripts/input_filter.pyc $1
